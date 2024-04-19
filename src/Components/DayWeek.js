fdb0320278ea1f766eff2631ae18c43d1d36e0e8
henriquepablo <pablo269704@gmail.com>
2024-04-19 20:41:23 -0300

    refactor: change component class DayWeek to component function DayWeek

--- src/Components/DayWeek.js
+++ src/Components/DayWeek.js
@@ -2,18 +2,16 @@ import React, { Component } from "react";
 import { View, Text, Image } from "react-native";
 import styles from '../styles/styleMain';
 
-export default class DayWeek extends Component {
-    render() {
+export default function DayWeek(props) {
         return (
             <View style={styles.dayWeek}>
-              <Text style={styles.textWeek}>{this.props.weekday}</Text>
+              <Text style={styles.textWeek}>{props.weekday}</Text>
               <Image source={require('../img/rain.png')}/>
-              <Text style={styles.percentRain}>{this.props.percentRain}%</Text>
+              <Text style={styles.percentRain}>{props.percentRain}%</Text>
               <Image source={require('../img/cloud-sun.png')}/>
               <Image source={require('../img/cloud-night.png')}/>
-              <Text style={styles.temp}>{this.props.tempMax}°C</Text>
-              <Text style={styles.temp}>{this.props.tempMin}°C</Text>
+              <Text style={styles.temp}>{props.tempMax}°C</Text>
+              <Text style={styles.temp}>{props.tempMin}°C</Text>
             </View>
         );
-    }
 }
