c6250711bf494d344f53b1f337ba3daeefe1a09f
henriquepablo <pablo269704@gmail.com>
2024-04-19 20:46:15 -0300

    refactor: change component class Loading to component function Loading

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import { View, Text, ActivityIndicator } from "react-native";
 import styleBody from "../styles/styleMain";
 
-export default class Loading extends Component {
-    render() {
+export default function Loading(props) {
+    
         return(
             <View style={styleBody.loading}> 
                 <ActivityIndicator size='large' color='#000' />
                 <Text>Carregando Informações</Text>
             </View>
         );
-    }
+    
 }
