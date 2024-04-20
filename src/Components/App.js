f5c3ab6dd450322a31938382b32720b0571d4a5f
henriquepablo <pablo269704@gmail.com>
2024-04-20 11:45:21 -0300

    feat: adding Context api

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,16 +2,22 @@ import React, { Component, useState } from "react";
 import { ScrollView, View } from "react-native";
 import Main from "./Main";
 import LinearGradient from "react-native-linear-gradient";
+import MyContext from "./Context";
 
 const App = () =>{
+
+  const [cityName, setCityName] = useState('');
+
      return(
-      <LinearGradient colors={['#5CBCDB', '#0096C7', '#0054B6', '#01377C']} start={{x:0.5, y:0}} style={{flex: 1}}>
-       <ScrollView>
-          <View >
-            <Main />
-          </View>
-       </ScrollView>
-      </LinearGradient>
+        <MyContext.Provider value={{cityName, setCityName}}>
+          <LinearGradient colors={['#5CBCDB', '#0096C7', '#0054B6', '#01377C']} start={{x:0.5, y:0}} style={{flex: 1}}>
+            <ScrollView>
+              <View >
+                <Main />
+              </View>
+            </ScrollView>
+          </LinearGradient>
+      </MyContext.Provider>
     );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import { Image, Text, TextInput, View } from "react-native";
 import styles from "../styles/styleHeader";
+import { useContext } from "react";
+import MyContext from "./Context";
 
 export default function Header(props) {
+
+  const {cityName, setCityName} = useContext(MyContext);
+
     return (
       <View style={styles.containerHeader}>
       
         <TextInput 
           placeholder="Pesquisar Local" 
           style={styles.input}
+          onSubmitEditing={text => setCityName(text.nativeEvent.text)}
         />
 
         <View style={styles.viewLocationHeader}>
