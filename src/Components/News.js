f4acccbf69bea9c6e6ddd6c72b80cc2c65d0544e
henriquepablo <pablo269704@gmail.com>
2024-04-27 14:58:08 -0300

    feat: implementing fetch with api of news and displaying some news

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,12 +1,43 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState} from "react";
+import { ScrollView, Text, View } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
 
 const News = () => {
+
+    const [newsItens, setNewsItens] = useState([]);
+
+    function loadInfo() {
+        fetch("http://servicodados.ibge.gov.br/api/v3/noticias/?tipo=noticias&qtd=50")
+        .then(response =>  response.json())
+        .then(json => {
+            
+            for (let i = 0; i < 5; i++) {
+                setNewsItens(json.items);
+            }
+            
+        }); 
+    }
+    
+    useEffect(() => {
+        loadInfo();
+    }, []); 
+    
     return (
-        <View>
-            <Text>
-                Hello Worlfd
-            </Text>
+        <View style={{flex: 1}}>
+            <ScrollView>
+            
+                <LinearGradient colors={['#5CBCDB', '#0096C7', '#0054B6', '#01377C']} start={{x:0.5, y:0}} style={{flex: 1}}>
+            
+                    {newsItens.map((item, index) => (
+                        <View key={index} style={{borderColor: '#000', borderStyle: 'dashed', borderWidth: 1}}>
+                            <Text>{item.titulo}</Text>
+                            <Text>{item.introducao}</Text>
+                        </View>
+                    ))}
+
+                </LinearGradient> 
+            
+            </ScrollView>
         </View>
     );
 }
